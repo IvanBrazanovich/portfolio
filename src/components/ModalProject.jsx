2ad1c4c90c2b3fa9a5790699cf0a73b85b82b38c
Ivan <ivanbrazas@gmail.com>
2023-03-24 21:40:58 -0300

    finished mobile styles

--- src/components/ModalProject.jsx
+++ src/components/ModalProject.jsx
@@ -2,12 +2,12 @@ import React from "react";
 
 const ModalProject = ({ item }) => {
   return (
-    <div className="box flex flex-col max-h-full overflow-y-auto overflow-x-hidden  py-5 px-12 rounded-lg justify-between  items-center bg-[#171717] ">
+    <div className="box flex flex-col max-h-full overflow-y-auto tl:max-[100vw] max-w-2xl   tl:p-1 py-5 px-12 rounded-lg justify-between  items-center bg-[#171717] ">
       <div className="info flex flex-col text-white">
         <p className="text-3xl self-center font-bold">{item.title}</p>
         <p className="my-3 font-bold text-xl ">{item.description}</p>
       </div>
-      <img src={item.img} className="max-w-2xl" />
+      <img src={item.img} className=" " />
       <div className="info flex flex-col text-white">
         <div className="mt-3 p-2 flex flex-col gap-1">
           <p className="max-w-xl whitespace-pre-wrap ">
--- src/components/ProjectCard.jsx
+++ src/components/ProjectCard.jsx
@@ -9,12 +9,12 @@ function ProjectCard({ item }) {
     <>
       <div className="img-box  w-[450px] lg2:w-auto mx-auto flex justify-center items-center relative overflow-hidden rounded-3xl">
         <img src={item.img} alt="" className="w-full" />
-        <div className="p absolute top-[-100%] left-0 h-full w-full text-center bg-[#b004b0b7] p-4 pt-20">
-          <h1 className="font-bold text-3xl">{item.title}</h1>
-          <p className="font-bold">{item.description}</p>
+        <div className="p absolute top-[-100%] left-0 h-full w-full text-center bg-[#b004b0b7] p-4 pt-14 tl:pt-5">
+          <h1 className="font-bold text-3xl tl:text-2xl">{item.title}</h1>
+          <p className="font-bold text-base">{item.description}</p>
           <button
             onClick={(e) => setModal(true)}
-            className="  text-white text-2xl mt-5 border-b-4 hover:bg-white hover:text-black transition-all p-3 rounded-md"
+            className="  text-white tl:text-xl text-2xl mt-5 border-b-4 hover:bg-white hover:text-black transition-all p-3  tl:p-1 rounded-md"
           >
             Know More
             <i className="fa-solid  fa-arrow-right text-lg  p-[2px] "></i>{" "}
